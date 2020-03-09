2e1afddff2da1de3fcf068149c70255a0962d92f
Brunomartins11 <brunocmartins@live.com.pt>
2020-03-09 16:49:16 +0000

    Start of service wip

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"user"
+
+	//	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var user = User{"bruno", "1234"}
+
+func connectDB() *mongo.Client {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	return client
+}
+
+func disconnectDB(client *mongo.Client) {
+	err := client.Disconnect(context.TODO())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connection to MongoDB closed.")
+}
+
+func main() {
+	client := connectDB()
+
+	collection := client.Database("auth").Collection("users")
+
+	// "Signin" and "Welcome" are the handlers that we will implement
+	http.HandleFunc("/signin", Signin)
+	http.HandleFunc("/welcome", Welcome)
+	http.HandleFunc("/refresh", Refresh)
+
+	// start the server on port 8000
+	log.Fatal(http.ListenAndServe(":8000", nil))
+
+	disconnectDB(client)
+}
